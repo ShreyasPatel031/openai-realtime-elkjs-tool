fd5b7f52fcc36975b20e3155686db2dd937c81ec
Shreyas Patel <shreyashfs@gmail.com>
2025-06-10 01:01:38 -0500

    Fix: Switch to export default syntax like working test endpoint

--- api/token.js
+++ api/token.js
@@ -1,4 +1,4 @@
-module.exports = async function handler(req, res) {
+export default function handler(req, res) {
   try {
     // Set CORS headers
     res.setHeader('Access-Control-Allow-Origin', '*');
